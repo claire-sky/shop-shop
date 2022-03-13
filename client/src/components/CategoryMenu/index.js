34e843e124e813f45424ce97b82820a80a58e612
Claire Sky <eclconnolly@gmail.com>
2022-03-13 16:29:49 -0700

    Merge with code snapshot

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -21,14 +21,14 @@ function CategoryMenu() {
         type: UPDATE_CATEGORIES,
         categories: categoryData.categories,
       });
-      categoryData.categories.forEach(category => {
+      categoryData.categories.forEach((category) => {
         idbPromise('categories', 'put', category);
       });
     } else if (!loading) {
-      idbPromise('categories', 'get').then(categories => {
+      idbPromise('categories', 'get').then((categories) => {
         dispatch({
           type: UPDATE_CATEGORIES,
-          categories: categories
+          categories: categories,
         });
       });
     }
