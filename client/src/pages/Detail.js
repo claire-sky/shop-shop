2505268dc495599078fb80e440507ea5db5d0784
Claire Sky <eclconnolly@gmail.com>
2022-03-13 10:21:43 -0700

    Build the Shopping Cart UI

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -5,7 +5,7 @@ import { useQuery } from '@apollo/client';
 import { useStoreContext } from '../utils/GlobalState';
 import { 
   ADD_TO_CART,
-  UPDATE_CART_ITEM,
+  UPDATE_CART_QUANTITY,
   REMOVE_FROM_CART,
   UPDATE_PRODUCTS,
 } from '../utils/actions';
@@ -21,7 +21,7 @@ function Detail() {
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const { products } = state;
+  const { products, cart } = state;
 
   useEffect(() => {
     if (products.length) {
@@ -35,9 +35,26 @@ function Detail() {
   }, [products, data, dispatch, id]);
 
   const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+  
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...currentProduct, purchaseQuantity: 1 }
+      });
+    }
+  };
+
+  const removeFromCart = () => {
     dispatch({
-      type: ADD_TO_CART,
-      product: { ...currentProduct, purchaseQuantity: 1 }
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id
     });
   };
 
@@ -54,7 +71,7 @@ function Detail() {
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
             <button onClick={addToCart}>Add to Cart</button>
-            <button>Remove from Cart</button>
+            <button disabled={!cart.find(p => p._id === currentProduct._id)} onClick={removeFromCart}>Remove from Cart</button>
           </p>
 
           <img
